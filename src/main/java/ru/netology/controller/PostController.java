929ffbb377d61fa85a2124af516e870bc0a300cd
Averianov Yuriy <averianovj@gmail.com>
2022-11-24 21:42:50 +0300

    refact (delete MyConfig)

--- src/main/java/ru/netology/controller/PostController.java
+++ src/main/java/ru/netology/controller/PostController.java
@@ -1,6 +1,7 @@
 package ru.netology.controller;
 
 import com.google.gson.Gson;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import ru.netology.model.Post;
 import ru.netology.service.PostService;
@@ -14,6 +15,7 @@ public class PostController {
     public static final String APPLICATION_JSON = "application/json";
     private final PostService service;
 
+    @Autowired
     public PostController(PostService service) {
         this.service = service;
     }
